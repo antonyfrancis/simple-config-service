4462f9e1071e6b6cb14b81acff6ff2558d7d8c4e
Antony Francis <anfrancis@bauer-media.com.au>
2015-09-02 02:54:10 +1000

    clean up the build script

--- build.cmd
+++ build.cmd
@@ -19,17 +19,9 @@ IF EXIST packages\KoreBuild goto run
 .nuget\NuGet.exe install KoreBuild -ExcludeVersion -o packages -nocache -pre -Source https://www.nuget.org/api/v2/;https://www.myget.org/F/aspnetvnext/
 .nuget\NuGet.exe install Sake -version 0.2 -o packages -ExcludeVersion -Source https://www.nuget.org/api/v2/;https://www.myget.org/F/aspnetvnext/
 
-IF "%SKIP_KRE_INSTALL%"=="1" goto run
-REM CALL packages\KoreBuild\build\dnvm upgrade -runtime CLR -x86 || set errorlevel=1
-REM CALL packages\KoreBuild\build\dnvm install 1.0.0-beta2 -runtime CoreCLR -x86 || set errorlevel=1
-
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
-CALL %USERPROFILE%\.dnx\bin\dnvm install latest -runtime CLR -x86 -alias default || set errorlevel=1
-CALL %USERPROFILE%\.dnx\bin\dnvm install latest -runtime CoreCLR -x86 || set errorlevel=1
-
-:run
-REM CALL packages\KoreBuild\build\dnvm use default -runtime CLR -x86 || set errorlevel=1
-REM packages\Sake\tools\Sake.exe -I packages\KoreBuild\build -f makefile.shade %*
+CALL %USERPROFILE%\.dnx\bin\dnvm install latest -runtime CLR -arch x86 -alias default || set errorlevel=1
+CALL %USERPROFILE%\.dnx\bin\dnvm install latest -runtime CoreCLR -arch x86 || set errorlevel=1
 
 CALL %USERPROFILE%\.dnx\bin\dnvm use default -runtime CLR -x86 || set errorlevel=1
 
