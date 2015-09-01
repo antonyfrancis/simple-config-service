400d7fe843c3a13358045d2ea604770a19706e86
Antony Francis <anfrancis@bauer-media.com.au>
2015-09-02 04:22:15 +1000

    fixed the assembly reference mismatches

--- Samples/AspNet5/AspNet5.Web/Startup.cs
+++ Samples/AspNet5/AspNet5.Web/Startup.cs
@@ -3,32 +3,35 @@ using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Framework.Configuration;
 using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Logging;
 using Microsoft.Framework.Runtime;
-using SimpleConfigService.AspNet5;
 
 namespace Sample.AspNet5.Web
 {
-    public class Startup
-    {
-	    public IConfiguration Configuration;
+	public class Startup
+	{
+		public IConfiguration Configuration;
 
-	    public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv)
-	    {
-			var builder = new ConfigurationBuilder(appEnv.ApplicationBasePath);
+		public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv, ILoggerFactory loggerFactory)
+		{
+			loggerFactory.MinimumLevel = LogLevel.Information;
+			loggerFactory.AddConsole();
+
+			var builder = new ConfigurationBuilder(appEnv.ApplicationBasePath)
+							.AddJsonFile("config.json")
+							.AddEnvironmentVariables();
 
 			Configuration = builder.Build();
 		}
 
 		public void ConfigureServices(IServiceCollection services)
-        {
-        }
+		{
+			services.AddMvc();
+		}
 
-        public void Configure(IApplicationBuilder app)
-        {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
-    }
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			app.UseMvcWithDefaultRoute();
+		}
+	}
 }
